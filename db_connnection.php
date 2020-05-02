3e8f4b0d339f2647e1bebe8d264f08e4c22983cd
Claude-Website <claudedelong1@gmail.com>
2020-05-02 19:16:11 +0100

    Update db_connnection.php

--- db_connnection.php
+++ db_connnection.php
@@ -1,12 +1,15 @@
 <?php
-$servername = "DELO";
+$servername = "localhost";
 $username = "cphkkqbggk";
 $password = "";
-$db = "cphkkqbggk";
+$db = "cphkkqbggk"
+
 // Create connection
-$conn = mysqli_connect($servername, $username, $password,$db);
+$conn = mysqli_connect($servername, $username, $password, $db);
+
 // Check connection
 if (!$conn) {
-   die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
+?>
